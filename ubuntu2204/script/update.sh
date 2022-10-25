99d012156675445bea2a88c8c18d4d2eafb35879
greatreject <116633783+greatreject@users.noreply.github.com>
2022-10-25 14:09:39 +0100

    Add files via upload

---
+++ ubuntu2204/script/update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -eux
+
+# Disable the release upgrader
+echo "==> Disabling the release upgrader"
+sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
+
+echo "==> Checking version of Ubuntu"
+. /etc/lsb-release
+
+if [[ $DISTRIB_RELEASE == 16.04 || $DISTRIB_RELEASE == 18.04 || $DISTRIB_RELEASE == 20.04 ]]; then
+    echo "==> Disabling periodic apt upgrades"
+    echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
+fi
+
+echo "==> Updating list of repositories"
+# apt-get update does not actually perform updates, it just downloads and indexes the list of packages
+apt-get -y update
+
+if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
+    echo "==> Performing dist-upgrade (all packages and kernel)"
+    apt-get -y dist-upgrade --force-yes
+    reboot
+    sleep 60
+fi
+
+# Ready for Kubernetes
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo echo "vm.swappiness=0" | sudo tee --append /etc/sysctl.conf
+sudo sysctl -p
+
+# Docker Install
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Install Kubernetes Tools
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-get install -y keepalived
+systemctl enable keepalived && systemctl start keepalived
+
+apt-get -y install net-tools
+apt-get -y install fish 
